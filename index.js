78fa72ec5c683e60a6b73547dd2f715e7a7cd055
user <user@MacBook-Air-Erulan.local>
2019-09-23 17:25:31 +0600

    commit

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const Telegraf = require('telegraf')
+// Подключаем библиотеку для работы с Telegram API в переменную
+var TelegramBot = require('node-telegram-bot-api');
 
-const bot = new Telegraf(906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM)
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
-bot.command('modern', ({ reply }) => reply('Yo'))
-bot.command('hipster', Telegraf.reply('λ'))
-bot.launch()
+// Устанавливаем токен, который выдавал нам бот
+var token = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM';
+// Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
+// Подробнее: https://core.telegram.org/bots/api#getupdates
+var bot = new TelegramBot(token, { polling: true });
+
+// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
+bot.onText(/\/echo (.+)/, function (msg, match) {
+    var fromId = msg.from.id; // Получаем ID отправителя
+    var resp = match[1]; // Получаем текст после /echo
+    bot.sendMessage(fromId, resp);
+});
+
+// Простая команда без параметров
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
+    // Фотография может быть: путь к файлу, поток (stream) или параметр file_id
+    var photo = 'cats.png'; // в папке с ботом должен быть файл "cats.png"
+    bot.sendPhoto(chatId, photo, { caption: 'Милые котята' });
+});
