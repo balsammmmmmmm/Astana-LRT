993d877fc2fa91d3c0cee7fd931be764f1413cdc
user <user@MacBook-Air-Erulan.local>
2019-09-23 16:17:09 +0600

    commit

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const token = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM';
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', msg -> {
-     bot.sendMessage(msg.chat.id, 'hello ${msg.from.first_name}')
-})
+bot.onText(/\/echo (.+)/, (msg, match) => {
+     // 'msg' is the received Message from Telegram
+     // 'match' is the result of executing the regexp above on the text content
+     // of the message
+   
+     const chatId = msg.chat.id;
+     const resp = match[1]; // the captured "whatever"
+   
+     // send back the matched "whatever" to the chat
+     bot.sendMessage(chatId, resp);
+   });
+   
+   // Listen for any kind of message. There are different kinds of
+   // messages.
+   bot.on('message', (msg) => {
+     const chatId = msg.chat.id;
+   
+     // send a message to the chat acknowledging receipt of their message
+     bot.sendMessage(chatId, 'Received your message');
+   });
