10e06b6f667868781bb65694c8aed03a40097697
user <user@MacBook-Air-Erulan.local>
2019-09-23 16:57:53 +0600

    commit

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
    
      const chatId = msg.chat.id;
      const resp = match[1]; // the captured "whatever"
-   
+     const exit = 'bye';
      // send back the matched "whatever" to the chat
-     bot.sendMessage(chatId, resp);
+     bot.sendMessage(chatId, resp, exit);
    });
    
    // Listen for any kind of message. There are different kinds of
