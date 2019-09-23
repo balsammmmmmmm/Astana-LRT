eb80ba980f65a36e3297376edd53c4d2851fccdb
user <user@MacBook-Air-Erulan.local>
2019-09-23 15:59:44 +0600

    commit

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const TOKEN = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM'
 
 const bot = new TelegramBot (process.env.TOKEN)
 
-bot.on('message', msg -> {
-     bot.sendMessage(msg.chat.id, 'hello ${msg.from.first_name}')
-})
+//bot.on('message', msg -> {
+//     bot.sendMessage(msg.chat.id, 'hello ${msg.from.first_name}')
+//})
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply(''))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
