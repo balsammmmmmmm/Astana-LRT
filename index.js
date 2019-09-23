e9739fdbd4fb985cefcff2bffde2695e06d43635
user <user@MacBook-Air-Erulan.local>
2019-09-23 16:25:02 +0600

    commit

--- index.js
+++ index.js
@@ -16,12 +16,3 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
      // send back the matched "whatever" to the chat
      bot.sendMessage(chatId, resp, curDate);
    });
-   
-   // Listen for any kind of message. There are different kinds of
-   // messages.
-   bot.on('message', (msg) => {
-     const chatId = msg.chat.id;
-   
-     // send a message to the chat acknowledging receipt of their message
-     bot.sendMessage(chatId, 'Received your message');
-   });
