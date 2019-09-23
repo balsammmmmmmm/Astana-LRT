9fb33615f24b4f6f9c0ebaa7d7fccca9a4b47c76
user <user@MacBook-Air-Erulan.local>
2019-09-23 17:30:38 +0600

    commit

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ var bot = new TelegramBot(token, { polling: true });
 
 // Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
 bot.onText(/\/echo (.+)/, function (msg, match) {
-    var fromId = msg.from.id; // Получаем ID отправителя
+    var chatId = msg.from.id; // Получаем ID отправителя
     var resp = match[1]; // Получаем текст после /echo
-    bot.sendMessage(fromId, resp);
+    bot.sendMessage(chatId, resp);
+});
+
+// Простая команда без параметров
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
+    // Фотография может быть: путь к файлу, поток (stream) или параметр file_id
+    var photo = 'cats.png'; // в папке с ботом должен быть файл "cats.png"
+    bot.sendPhoto(chatId, photo, { caption: 'Милые котята' });
 });
