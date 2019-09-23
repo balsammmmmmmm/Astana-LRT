5720e6fb089fd474930e5c5960e9654a43d5f770
user <user@MacBook-Air-Erulan.local>
2019-09-23 18:40:30 +0600

    commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Подключаем библиотеку для работы с Telegram API в переменную
-var TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
 
 // Устанавливаем токен, который выдавал нам бот
-var token = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM';
+const token = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM';
 // Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
 // Подробнее: https://core.telegram.org/bots/api#getupdates
-var bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { polling: true });
 
 // Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
 bot.on('message', (msg, match) =>{
-     var chatId = msg.chat.id;
-     var resp = match[1];
+     const chatId = msg.chat.id;
+     const resp = match[1];
      bot.sendMessage(chatId, `Lorem Ipsum has been the industry's standard "${resp}" dummy text ever since the 1500s, when an unknown printer took a galley of ${'ТУТ ДАТА КОТОРУЮ ХОТИТЕ'} type and scrambled it to make a type specimen book.`);
      
 })
