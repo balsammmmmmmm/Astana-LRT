b8135575de41c1d5403d24730d6da2ea3ecae6f3
user <user@MacBook-Air-Erulan.local>
2019-09-23 18:06:32 +0600

    commit

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var token = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM';
 var bot = new TelegramBot(token, { polling: true });
 
 // Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
-bot.onText(/\/echo (.+)/, function (msg, match) {
-    var chatId = msg.from.id; // Получаем ID отправителя
+bot.onText(/\/echo (.+)/, function (msg, match, ) {
+    var chatId = msg.chat.id; // Получаем ID отправителя
     var resp = match[1]; // Получаем текст после /echo
     bot.sendMessage(chatId, resp);
 });
