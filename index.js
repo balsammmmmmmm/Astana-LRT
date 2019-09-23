4a60b1458017a28aba1af78a9c66dd71eef92e45
user <user@MacBook-Air-Erulan.local>
2019-09-23 16:10:53 +0600

    commit

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
 
-const TOKEN = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM'
+const token = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM';
 
-const bot = new TelegramBot (process.env.TOKEN)
+const bot = new TelegramBot(token, {polling: true});
 
-//bot.on('message', msg -> {
-//     bot.sendMessage(msg.chat.id, 'hello ${msg.from.first_name}')
-//})
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply(''))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+bot.on('message', msg -> {
+     bot.sendMessage(msg.chat.id, 'hello ${msg.from.first_name}')
+})
