61671abdb2ad2a89233b0daa5c7398fcd1fc12c6
user <user@MacBook-Air-Erulan.local>
2019-09-23 18:33:03 +0600

    commit

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ var token = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM';
 var bot = new TelegramBot(token, { polling: true });
 
 // Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
-bot.onText('message', function (msg, match, ) {
+bot.onText(/\/echo (.+)/, function (msg, match) {
     var chatId = msg.chat.id; // Получаем ID отправителя
     var resp = match[1]; // Получаем текст после /echo
     bot.sendMessage(chatId, resp);
 });
+
+bot.on('message', (msg, match) =>{
+     var chatId = msg.chat.id;
+     var resp = match[1];
+     bot.sendMessage(chatId, `Lorem Ipsum has been the industry's standard ${resp} dummy text ever since the 1500s, when an unknown printer took a galley of ${'ТУТ ДАТА КОТОРУЮ ХОТИТЕ'} type and scrambled it to make a type specimen book.`);
+     
+})
