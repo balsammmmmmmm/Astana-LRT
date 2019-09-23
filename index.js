f634812e59fa727c0e2acd9cf4e359362359dc08
user <user@MacBook-Air-Erulan.local>
2019-09-23 17:14:53 +0600

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf(process.env.'906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM')
+const bot = new Telegraf(process.env.906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM)
 bot.command('oldschool', (ctx) => ctx.reply('Hello'))
 bot.command('modern', ({ reply }) => reply('Yo'))
 bot.command('hipster', Telegraf.reply('λ'))
