e8e5dbef8c88b62ff1c1afaa87ac28ed085355ce
user <user@MacBook-Air-Erulan.local>
2019-09-23 18:42:16 +0600

    commit

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const bot = new TelegramBot(token, { polling: true });
 bot.on('message', (msg, match) =>{
      const chatId = msg.chat.id;
      const resp = match[1];
-     bot.sendMessage(chatId, `Lorem Ipsum has been the industry's standard "${resp}" dummy text ever since the 1500s, when an unknown printer took a galley of ${'ТУТ ДАТА КОТОРУЮ ХОТИТЕ'} type and scrambled it to make a type specimen book.`);
+     bot.sendMessage(chatId, `Lorem Ipsum has been the industry's standard '${resp}' dummy text ever since the 1500s, when an unknown printer took a galley of ${'ТУТ ДАТА КОТОРУЮ ХОТИТЕ'} type and scrambled it to make a type specimen book.`);
      
 })
