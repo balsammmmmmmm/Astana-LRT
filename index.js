d94cbfded91e294d562b97d3fbc6ec022beb827f
user <user@MacBook-Air-Erulan.local>
2019-09-23 17:26:45 +0600

    commit

--- index.js
+++ index.js
@@ -13,11 +13,3 @@ bot.onText(/\/echo (.+)/, function (msg, match) {
     var resp = match[1]; // Получаем текст после /echo
     bot.sendMessage(fromId, resp);
 });
-
-// Простая команда без параметров
-bot.on('message', function (msg) {
-    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
-    // Фотография может быть: путь к файлу, поток (stream) или параметр file_id
-    var photo = 'cats.png'; // в папке с ботом должен быть файл "cats.png"
-    bot.sendPhoto(chatId, photo, { caption: 'Милые котята' });
-});
