85361cd6a6c7039c2603d7a78606d030af907326
user <user@MacBook-Air-Erulan.local>
2019-09-23 15:57:25 +0600

    commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api')
 
+const TOKEN = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM'
 
-const bot = new TelegramBot (906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM , {polling: true})
+const bot = new TelegramBot (process.env.TOKEN)
 
 bot.on('message', msg -> {
      bot.sendMessage(msg.chat.id, 'hello ${msg.from.first_name}')
