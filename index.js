0a7f26f66835548f713036b3a878f854f4d5d86d
Belavor <sovetbergene@gmail.com>
2019-09-24 18:15:39 +0600

    new

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('')
+})
+
 // Подключаем библиотеку для работы с Telegram API в переменную
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require('node-telegram-bot-api');
