c1ad9ed5bbe6400b749e4707c64f34bf8512dc99
Belavor <sovetbergene@gmail.com>
2019-09-24 18:55:13 +0600

    a

--- index.js
+++ index.js
@@ -31,6 +31,9 @@
 // https://smsbus.kz/cd.jsp?id=0078382222`);
      
 // })
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('');
+});
 process.env["NTBA_FIX_319"] = 1;
 const TelegramBot = require('node-telegram-bot-api');
 const token = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM';
