12123809dccd682dc69133cbd666938c56d42abd
Belavor <sovetbergene@gmail.com>
2019-09-24 18:59:51 +0600

    a

--- index.js
+++ index.js
@@ -43,13 +43,15 @@ bot.onText(/\/echo (.+)/, function(msg, match) {
          const chatId = msg.chat.id;
          const resp = match[1];
          const curTime = new Date().getUTCHours() + ':' + new Date().getUTCMinutes() + ':' + new Date().getUTCSeconds();
+         const random = Math.Random().getRandom();
+         
          bot.sendMessage(chatId,
-            `БИЛЕТ: 0078:38:2222
-            СУММА: 90 ТГ.
-            Дата: ${curTime}
-            Транспорт: ${resp}
-            ТЕЛ: 77769097977
-            ТРАНЗАКЦИЯ: 338532222
-            ТОО АСТАНА LRT
-            https://smsbus.kz/cd.jsp?id=0078382222`);
+            `БИЛЕТ: 0078:38:${random}
+СУММА: 90 ТГ.
+Дата: ${curTime}
+Транспорт: ${resp}
+ТЕЛ: 77769097977
+ТРАНЗАКЦИЯ: 33853${random}
+ТОО АСТАНА LRT
+https://smsbus.kz/cd.jsp?id=007838${random}2`);
          });
