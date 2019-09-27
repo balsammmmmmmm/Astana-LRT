2b9205eeac160867055ed7569f5c5920576289f0
Belavor <sovetbergene@gmail.com>
2019-09-27 09:44:13 +0600

    a

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM';
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/start (.+)/), function(msg) {
-    bot.sendMessage(msg.chat.id, `Чтобы оплатить напишите /оплата "номер траспорта"`);
+bot.onText(/\/start (.+)/), function(msg, match) {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, `Чтобы оплатить напишите /оплата "номер траспорта"`);
 };
 bot.onText(/\/оплата (.+)/, function(msg, match) {
          const chatId = msg.chat.id;
