6051d1179b98c2f2ee362d51aa19d22fd2cf8f8f
Belavor <sovetbergene@gmail.com>
2019-09-30 23:38:11 +0600

    a

--- index.js
+++ index.js
@@ -11,16 +11,28 @@ const button = {
     parse_mode: 'markdown',
     disable_web_page_preview: false,
     reply_markup: JSON.stringify({
-        keyboard: [[{text: `Номер Автобуса`, callback_data:`${match}`},]],
+        keyboard: [[{text: `Номер Автобуса`, callback_data:'num'},]],
         resize_keyboard: true
     })
  };
-    bot.sendMessage(msg.from.id, `Чтобы оплатить напишите /оплата "номер траспорта"`, button);
+    bot.sendMessage(msg.from.id, button);
+});
+bot.onText(/num/, function(msg, match){
+    const auto = match[1];
+    bot.sendMessage(msg.from.id,
+`БИЛЕТ: ${randomFirst}:38:${randomSecond}
+СУММА: 90 ТГ.
+Дата: ${curTime}
+Транспорт: ${resp}
+ТЕЛ: 77769097977
+ТРАНЗАКЦИЯ: 33853${randomSecond}
+ТОО АСТАНА LRT
+https://smsbus.kz/cd.jsp?id=${randomFirst}38${randomSecond}` );
 });
  
 bot.onText(/\/оплата (.+)/, function(msg, match) {
          const fromId = msg.from.id;
-         const resp = match[1]; 
+         const resp = match[1];
          const randomFirst = Math.floor(1000 + Math.random() * 9000);
          const randomSecond = Math.floor(1000 + Math.random() * 9000);
          const curTime = new Date();
