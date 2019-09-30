0dfb2976128daf2fcf12ea608b8adaa3d10d0553
Belavor <sovetbergene@gmail.com>
2019-10-01 00:40:32 +0600

    a

--- index.js
+++ index.js
@@ -42,32 +42,32 @@ const button = {
                             text: 'Вторник',
                             callback_data: JSON.stringify({
                                 'command': 'sch',
-                                'base': 'tue'
+                                callback_data: 'tue'
                             })
                         },
                         {
                             text: 'Среда',
                             callback_data: JSON.stringify({
                                 'command': 'sch',
-                                'base': 'wed'
+                                callback_data: 'wed'
                             })
                         },                        {
                             text: 'Четверг',
                             callback_data: JSON.stringify({
                                 'command': 'sch',
-                                'base': 'thu'
+                                callback_data: 'thu'
                             })
                         },                        {
                             text: 'Пятница',
                             callback_data: JSON.stringify({
                                 'command': 'sch',
-                                'base': 'fri'
+                                callback_data: 'fri'
                             })
                         },                        {
                             text: 'Субота',
                             callback_data: JSON.stringify({
                                 'command': 'sch',
-                                'base': 'sat'
+                                callback_data: 'sat'
                             })
                         }
                     ]
