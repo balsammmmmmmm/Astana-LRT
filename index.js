3dbc5dfaaf3da425267c6dc8425b27bafc1f4ae2
Belavor <sovetbergene@gmail.com>
2019-10-10 21:50:32 +0600

    a

--- index.js
+++ index.js
@@ -109,10 +109,10 @@ bot.onText(/pay (.+)/, function(msg, match) {
     const response = match[1].slice(-3);
     const randomFirst = Math.floor(10 + Math.random() * 90);
     const randomSecond = Math.floor(1000 + Math.random() * 9000);
-    const curTime = new Date();
+    const curTime = new Date(); 
     curTime.setSeconds(curTime.getSeconds() + 21600);
+    curTime.toString().substring(7,24);
 
-    bot.sendMessage(fromId,
-`БИЛЕТ: 00${randomFirst}:38:${randomSecond}\nСУММА: 90 ТГ.\nДата: ${curTime}\nТранспорт: ${resp} A${response}\nТЕЛ: 77769097977\nТРАНЗАКЦИЯ: 33853${randomSecond}\nТОО АСТАНА LRT\nhttps://smsbus.kz/cd.jsp?id=00${randomFirst}38${randomSecond}`);
+    bot.sendMessage(fromId,`БИЛЕТ: 00${randomFirst}:38:${randomSecond}\nСУММА: 90 ТГ.\nДата: ${curTime}\nТранспорт: ${resp} A${response}\nТЕЛ: 77769097977\nТРАНЗАКЦИЯ: 33853${randomSecond}\nТОО АСТАНА LRT\nhttps://smsbus.kz/cd.jsp?id=00${randomFirst}38${randomSecond}`);
     });
  
