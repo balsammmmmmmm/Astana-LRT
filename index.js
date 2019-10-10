e494ff3a52f75afeacf51194a5123adfe12ff243
Belavor <sovetbergene@gmail.com>
2019-10-10 21:05:33 +0600

    a

--- index.js
+++ index.js
@@ -110,14 +110,14 @@ bot.onText(/pay (.+)/, function(msg, match) {
     const randomFirst = Math.floor(1000 + Math.random() * 9000);
     const randomSecond = Math.floor(1000 + Math.random() * 9000);
     const tm = new Date();
-    /*const curTime = '';
+    const curTime = '';
     curTime += "" + tm.getDate() + "." + (tm.getMonth() + 1)
     + "." + tm.getFullYear();
-    curTime += " " + tm.getHours() + ":"
+    /*curTime += " " + tm.getHours() + ":"
        + tm.getMinutes() + ":" + tm.setSeconds(curTime.getSeconds() + 21600) + "";*/
     //curTime.setSeconds(curTime.getSeconds() + 21600);
 
     bot.sendMessage(fromId,
-`БИЛЕТ: ${randomFirst}:38:${randomSecond}\nСУММА: 90 ТГ.\nДата: ${tm}\nТранспорт: ${resp} A${response}\nТЕЛ: 77769097977\nТРАНЗАКЦИЯ: 33853${randomSecond}\nТОО АСТАНА LRT\nhttps://smsbus.kz/cd.jsp?id=${randomFirst}38${randomSecond}`);
+`БИЛЕТ: ${randomFirst}:38:${randomSecond}\nСУММА: 90 ТГ.\nДата: ${curTime}\nТранспорт: ${resp} A${response}\nТЕЛ: 77769097977\nТРАНЗАКЦИЯ: 33853${randomSecond}\nТОО АСТАНА LRT\nhttps://smsbus.kz/cd.jsp?id=${randomFirst}38${randomSecond}`);
     });
  
