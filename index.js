a137ff1e31c322bdf50ab0158a63035536d58d7e
Belavor <sovetbergene@gmail.com>
2019-10-25 22:36:00 +0600

    l

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
-    res.end('');
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function (req, res) {
+  res.end('');
 });
 process.env["NTBA_FIX_319"] = 1;
 const TelegramBot = require('node-telegram-bot-api');
 const token = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM';
-const bot = new TelegramBot(token, {polling: true});
-const botw = require('./scrapper.js'); 
+const bot = new TelegramBot(token, {
+  polling: true
+});
+const botw = require('./scrapper.js');
 
 //оплата за проезд
-bot.onText(/\/pay(.+)/, function(msg, match) {
-    let fromId = msg.from.id;
-    let resp = match[1];
-    let response = match[1].slice(-3);
-    let randomFirst = Math.floor(100 + Math.random() * 900);
-    let randomSecond = Math.floor(1000 + Math.random() * 9000);
-    const curTime = new Date(); 
-    curTime.setSeconds(curTime.getSeconds() + 21600);
-    const ct = curTime.toString().substring(4,24);
+bot.onText(/\/pay(.+)/, function (msg, match) {
+  let fromId = msg.from.id;
+  let resp = match[1];
+  let response = match[1].slice(-3);
+  let randomFirst = Math.floor(100 + Math.random() * 900);
+  let randomSecond = Math.floor(1000 + Math.random() * 9000);
+  const curTime = new Date();
+  curTime.setSeconds(curTime.getSeconds() + 21600);
+  const ct = curTime.toString().substring(4, 24);
 
-    bot.sendMessage(fromId,`БИЛЕТ: 0${randomFirst}:38:${randomSecond}\nСУММА: 90 ТГ.\nДата: ${ct}\nТранспорт: ${resp} A${response}\nТЕЛ: 77769097977\nТРАНЗАКЦИЯ: 33853${randomSecond}\nТОО АСТАНА LRT\nhttps://smsbus.kz/cd.jsp?id=00${randomFirst}38${randomSecond}`);
-    });
- 
+  bot.sendMessage(fromId, `БИЛЕТ: 0${randomFirst}:38:${randomSecond}\nСУММА: 90 ТГ.\nДата: ${ct}\nТранспорт: ${resp} A${response}\nТЕЛ: 77769097977\nТРАНЗАКЦИЯ: 33853${randomSecond}\nТОО АСТАНА LRT\nhttps://smsbus.kz/cd.jsp?id=00${randomFirst}38${randomSecond}`);
+});
+
 //grade
 
-bot.onText(/\/gr(.+)/, function(msg, match) {
+bot.onText(/\/gr(.+)/, function (msg, match) {
 
   let chatId = msg.chat.id;
 
-  (async() => {
-  
+  (async () => {
+
     await botw.initialize();
-  
+
     await botw.login('020924550859', 'Ch@rlycharly2002lool');
-  
+
     await botw.grade();
-    
+
     debugger;
-  
+
   })();
-  
+
   console.log(botw);
 
 
-  bot.sendPhoto(chatId, photo, { caption: 'grades.png' });
+  bot.sendPhoto(chatId, photo, {
+    caption: 'grades.png'
+  });
 });
--- scrapper.js
+++ scrapper.js
@@ -8,21 +8,34 @@ const platonus = {
 
      initialize: async () => {
           platonus.browser = await puppeteer.launch({
-               args: ['--no-sandbox']
+               args: ['--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--single-process'
+               ]
           });
           platonus.page = await platonus.browser.newPage();
-          await platonus.page.setViewport({width: 1000, height: 500});
-     }, 
-     
-     login: async (username, password) => { 
-          await platonus.page.goto(eduEnu, { waitUntil: 'networkidle2' });
+          await platonus.page.setViewport({
+               width: 1000,
+               height: 500
+          });
+     },
+
+     login: async (username, password) => {
+          await platonus.page.goto(eduEnu, {
+               waitUntil: 'networkidle2'
+          });
           //await platonus.page.waitFor(1000);
           // let closeButton = await platonus.page.type('div[class="backdrop-close"]');
           // await closeButton[0].click();
-          
-          
-          await platonus.page.type('input[name = "iin"]', username, {delay: 1});
-          await platonus.page.type('input[name = "password"]', password, {delay: 1});
+
+
+          await platonus.page.type('input[name = "iin"]', username, {
+               delay: 1
+          });
+          await platonus.page.type('input[name = "password"]', password, {
+               delay: 1
+          });
 
           let loginButton = await platonus.page.$x("//button[contains(text(), 'Кіру')]");
           await loginButton[0].click();
@@ -30,9 +43,14 @@ const platonus = {
      },
 
      grade: async () => {
-          await platonus.page.goto(grades, { waitUntil: 'networkidle2' });
-          await platonus.page.screenshot({path: 'grades.png', fullPage: true});
-          platonus.browser.close();
+          await platonus.page.goto(grades, {
+               waitUntil: 'networkidle2'
+          });
+          await platonus.page.screenshot({
+               path: 'platonus.png',
+               fullPage: true
+          });
+          await browser.close();
 
      }
 }
