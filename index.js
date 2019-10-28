f23f170220fd8e9e2bf3fde159732844213d6527
belavor <39500699+belavor@users.noreply.github.com>
2019-10-28 15:30:39 +0600

    A

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bot = new TelegramBot(token, {
 const botw = require('./scrapper.js');
 
 //оплата за проезд
-bot.onText(/\/pay(.+)/, function (msg, match) {
+bot.onText(/(.+)/, function (msg, match) {
   let fromId = msg.from.id;
   let resp = match[1];
   let response = match[1].slice(-3);
