ddfc70b32e0c403f2bfc21909a8174f0c56b0585
Belavor <sovetbergene@gmail.com>
2019-12-15 20:17:34 +0600

    a

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ bot.onText(/(.+)/, function (msg, match) {
 // platonus scrapper
 bot.onText(/\/2(.+)/, function (msg, match) {
   let chatId = msg.chat.id;
-  let photo = 'grades.png';
+  let photo = 'https://avatars.mds.yandex.net/get-pdb/2308976/1bb02713-ec93-4fbf-86e8-bb2a3ae4e18c/s1200';
   require('./head.js');
   bot.sendPhoto({chatId, photo, caption: 'tests' }).then(function(data)
   {
