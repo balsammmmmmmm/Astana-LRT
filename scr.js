b7e40f0c67af0152879b10a17b6487c21b455789
Belavor <sovetbergene@gmail.com>
2019-12-20 13:21:41 +0600

    1

--- scr.js
+++ scr.js
@@ -1,18 +1,28 @@
 const puppeteer = require('puppeteer');
-  (async() => {
-     let scr = 'https://trashbox.ru/link/which-iphone-to-buy-for-2020'
+(async () => {
+  let scr = 'https://trashbox.ru/link/which-iphone-to-buy-for-2020'
 
 
-     const browser = await puppeteer.launch({headless:false});
-     const page = await browser.newPage();
-     
-     await page.goto(scr, { waituntil: 'networkidle2'});
-     title = await page.$eval(('.poop'), node => node.innerText.trim());
-     console.log(title);
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--single-process'
+    ],
+  });
+  const page = await browser.newPage();
 
-      await browser.close();
-  })().then(() => {
-    console.log('Process done!!!');
+  await page.goto(scr, {
+    waituntil: 'networkidle2'
+  });
+  title = await page.$eval(('.poop'), node => node.innerText.trim());
+  console.log(title);
+
+  await browser.close();
+})().then(() => {
+  console.log('Process done!!!');
 }).catch((err) => {
-    console.log(err)
+  console.log(err)
 });
