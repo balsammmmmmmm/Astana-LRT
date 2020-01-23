3d2af3bda4f901f5abc872f1473fea6432234975
Belavor <sovetbergene@gmail.com>
2020-01-23 23:44:14 +0600

    a

--- index.js
+++ index.js
@@ -7,22 +7,33 @@ const token = '906646620:AAHXBYRUtBgPMm2UYNquCOqv3HGhj4LFdMM';
 const bot = new TelegramBot(token, {
   polling: true
 });
-// bot.on('message', msg => {
-//   let chatId = msg.chat.id;
-//   reply_markup: {
-//     keyboard: [
-//       [A],
-//       [B],
-//       [C],
-//       [D],
-//       [E],
-//       [F],
-//       [G],
-//       [H],
-//     ]
-//   }
-// });
 
+const keyboard =[
+  [{text: 'A',
+    callback_data: 'A'
+},
+  {text: 'B',
+    callback_data: 'B'
+},
+  {text: 'C',
+    callback_data: 'C'
+}],
+  [{text: 'D',
+    callback_data: 'D'
+},
+  {text: 'E',
+    callback_data: 'E'
+},
+  {text: 'F',
+    callback_data: 'F'
+}],
+  [{text: 'G',
+    callback_data: 'G'
+},
+  {text: 'H',
+    callback_data: 'H'
+}]
+];
 //оплата за проезд
 bot.onText(/(.+)/, function (msg, match) {
   let chatId = msg.chat.id;
@@ -33,39 +44,7 @@ bot.onText(/(.+)/, function (msg, match) {
   let randomSecond = Math.floor(1000 + Math.random() * 9000);
   const curTime = new Date();
   const ct = curTime.toString().substring(4, 24);
-  reply_markup: {
-    keyboard: [
-      [A, B, C],
-      [D, E, F],
-      [G, H],
-      [Закрыть]
-    ]
-  }
+
+
   bot.sendMessage(chatId, `БИЛЕТ: 0${randomFirst}:38:${randomSecond}\nСУММА: 90 ТГ.\nДата: ${ct}\nТранспорт: ${resp} ${a}${response}\nТЕЛ: 77769097977\nТРАНЗАКЦИЯ: 33853${randomSecond}\nТОО АСТАНА LRT\nhttps://smsbus.kz/cd.jsp?id=00${randomFirst}38${randomSecond}`);
-});
-
-// platonus scrapper
-// bot.onText(/\/2(.+)/, function (msg, match) {
-//   let chatId = msg.chat.id;
-//   let photo = 'https://contacts.google.com/?hl=ru&tab=iC1&authuser=0';
-//   require('./head.js');
-//   bot.sendPhoto({chatId, photo, caption: 'tests' }).then(function(data)
-//   {
-//       console.log(data)});
-//   bot.sendMessage(chatId, 'test');
-// });
-
-// // inst bot 
-// bot.onText(/\/2(.+)/, function(msg, match) {
-//   let chatId = msg.chat.id;
-//   require('./head.js');
-
-//   bot.sendMessage(chatId, 'check inst')
-// });
-
-// puppeteer scraping
-bot.onText(/\/1/, function (msg, ) {
-  let chatId = msg.chat.id;
-  require('./head.js');
-  bot.sendMessage(chatId, `${title}`);
 });
